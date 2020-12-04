7abee83439dc900ca6bf40b045c2e3745fe09b37
Yoshimi0824 <yoshi.gaze.0824@gmail.com>
2020-12-04 15:00:59 +0900

    ログインの条件分岐

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,11 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(user_id: current_user.id, review_id: params[:review_id])
-    @likes = Like.where(review_id: @review.id)
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, review_id: params[:review_id])
     @like.destroy
-    @likes = Like.where(review_id: @review.id)
   end
 
   private
